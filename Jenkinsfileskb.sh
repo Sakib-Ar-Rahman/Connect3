efcadb415e4af82ad064f5e13d8e94706fee7159
Sakib-Ar-Rahman <sakib.ar.rahman@gmail.com>
2017-02-22 12:11:15 -0500

    Changed file name, corrected path

---
+++ Jenkinsfileskb.sh
@@ -0,0 +1,40 @@
+#!groovy
+
+// Android Jenkinsfile
+
+node {
+    stage('Build') {
+        checkout scm
+        sh '''
+
+        echo 'Checking out gradle wrapper'
+
+        git checkout gradle
+
+        # enabling 'Jenkins' mode in gradle:
+        sed -i -e 's/def automatedBuild *= *false/def automatedBuild = true/g' build.gradle
+
+        
+        
+
+        ./gradlew wrapper # in case someone updates the build-tools
+        echo 'Building...'
+        pwd
+        ./gradlew clean
+        ./gradlew assemble
+
+
+
+        '''
+    }
+    stage('Test') {
+    	echo 'Testing....'
+
+        
+
+
+    }
+    stage('Deploy') {
+        echo 'Deploying....'
+    }
+}
