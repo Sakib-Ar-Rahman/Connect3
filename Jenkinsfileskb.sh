500fa42ab5273757bce12b9792be0616684de168
Sakib-Ar-Rahman <sakib.ar.rahman@gmail.com>
2017-02-22 14:55:13 -0500

    Added gradle wrapper code

--- Jenkinsfileskb.sh
+++ Jenkinsfileskb.sh
@@ -2,39 +2,46 @@
 
 // Android Jenkinsfile
 
+stage 'Get_External_Shell_Scripts'
 node {
-    stage('Build') {
-        checkout scm
-        sh '''
 
-        # echo 'Checking out gradle wrapper'
+    try{
+        stage('Build') {
+            checkout scm
+            
+            sh '''
+            echo 'Checking out gradle wrapper'
+            cd tictactoe
+            git checkout gradle
 
-        # git checkout gradle
+            # enabling 'Jenkins' mode in gradle:
+            sed -i -e 's/def automatedBuild *= *false/def automatedBuild = true/g' build.gradle
 
-        # # enabling 'Jenkins' mode in gradle:
-        # sed -i -e 's/def automatedBuild *= *false/def automatedBuild = true/g' build.gradle
-
-        
-        
-
-        # ./gradlew wrapper # in case someone updates the build-tools
-        # echo 'Building...'
-        # pwd
-        # ./gradlew clean
-        # ./gradlew assemble
-
+            ./gradlew wrapper # in case someone updates the build-tools
+            echo 'Building...'
+            pwd
+            ./gradlew clean
+            ./gradlew assemble
 
 
-        '''
+
+            '''
+        }
+        stage('Test') {
+        	echo 'Testing....'
+
+            
+
+
+        }
+        stage('Deploy') {
+            echo 'Deploying....'
+        }
     }
-    stage('Test') {
-    	echo 'Testing....'
-
-        
-
-
+    catch(e){
+      slackSend color: "FF0000", message: "JENKINS ALERT! - BUILD FAILURE '${env.JOB_NAME}  [${env.BUILD_NUMBER}]' \n ${env.BUILD_TIMESTAMP} \n ${env.BUILD_URL}"
+      currentBuild.result = "FAILED"
+      throw e
     }
-    stage('Deploy') {
-        echo 'Deploying....'
-    }
+
 }
