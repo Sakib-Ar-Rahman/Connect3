c0e006514610a03f4bc3fcd85c4219f8236138de
Sakib-Ar-Rahman <sakib.ar.rahman@gmail.com>
2017-02-23 12:06:30 -0500

    Added credential binding

--- Jenkinsfileskb.sh
+++ Jenkinsfileskb.sh
@@ -4,6 +4,8 @@
 
 stage 'Get_External_Shell_Scripts'
 node {
+
+    withCredentials([usernameColonPassword(credentialsId: 'mylogin', variable: 'USERPASS')]) {
     try{
         stage('Build') {
             checkout scm
@@ -39,18 +41,15 @@ node {
         stage('Send to Veracode'){
             echo 'Send to Veracode...'
 
-        
-
-      veracode applicationName: 'UGO - UGO Digital Wallet - Android', canFailJob: true, createSandbox: true, criticality: 'VeryHigh', debug: true, fileNamePattern: '', pHost: '', pPassword: '', pUser: '', replacementPattern: '', sandboxName: 'Sandbox Testing Sakib', scanExcludesPattern: '', scanIncludesPattern: '', scanName: 'Build-$buildnumber-Time-$timestamp', uploadExcludesPattern: '', uploadIncludesPattern: '**/**.jar, **/**.apk', useIDkey: true, vid: '19705c94f457345b9b9a809015525fcb', vkey: '7dd5d6f7c35d9cab66eaf4ee40c93a0ebf26b60ba505fadedf78c64452cd7d914ddd4e231e098a2bf64f70244750a62c58c3318bc5f5226186dd71fcedda4620', vpassword: '', vuser: ''
-
-  
-
+            veracode applicationName: 'UGO - UGO Digital Wallet - Android', canFailJob: true, createSandbox: true, criticality: 'VeryHigh', debug: true, fileNamePattern: '', pHost: '', pPassword: '', pUser: '', replacementPattern: '', sandboxName: 'Sandbox Testing Sakib', scanExcludesPattern: '', scanIncludesPattern: '', scanName: 'Build-$buildnumber-Time-$timestamp', uploadExcludesPattern: '', uploadIncludesPattern: '**/**.jar, **/**.apk', useIDkey: true, vid: '19705c94f457345b9b9a809015525fcb', vkey: '7dd5d6f7c35d9cab66eaf4ee40c93a0ebf26b60ba505fadedf78c64452cd7d914ddd4e231e098a2bf64f70244750a62c58c3318bc5f5226186dd71fcedda4620', vpassword: '', vuser: ''
         }
+
     }
+    
     catch(e){
       slackSend color: "FF0000", message: "JENKINS ALERT! - BUILD FAILURE '${env.JOB_NAME}  [${env.BUILD_NUMBER}]' \n ${env.BUILD_TIMESTAMP} \n ${env.BUILD_URL}"
       currentBuild.result = "FAILED"
       throw e
     }
-
+}
 }
