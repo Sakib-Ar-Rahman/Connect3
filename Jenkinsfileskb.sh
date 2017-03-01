51866e2e1c3c4a3d65e62d8535ae720ca074395b
Sakib-Ar-Rahman <sakib.ar.rahman@gmail.com>
2017-03-01 12:13:25 -0500

    Minor Tweaks

--- Jenkinsfileskb.sh
+++ Jenkinsfileskb.sh
@@ -49,6 +49,8 @@ node {
             checkout scm
             
             sh '''
+            echo 'Existing Branch name'
+            echo $BRANCH_NAME
 
             if [ -z "$BRANCH_NAME" ]; then
                 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
