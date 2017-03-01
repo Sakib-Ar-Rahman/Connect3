d8f9e26cc454b0a979ab78acb1ab723d4e6ee69e
Sakib-Ar-Rahman <sakib.ar.rahman@gmail.com>
2017-03-01 10:06:07 -0500

    Changed path to build

--- Jenkinsfileskb.sh
+++ Jenkinsfileskb.sh
@@ -43,13 +43,23 @@ node {
         stage('Send to Veracode'){
 
             echo 'Naming branch...'
+            echo 'Send to Veracode...'
+            echo 'Naming branch...'
             echo $BRANCH_NAME
+            echo 'Setting name of file'
+            sh '''
+
+            if [ -z "$BRANCH_NAME" ]; then
+                BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+            fi
+            for FILENAME in build/outputs/apk/*.apk
+                do NEWFILENAME=$FILENAME"_"$BRANCH_NAME
+            done
 
 
-            
-            echo $BRANCH_NAME
-            echo $NEWFILENAME
+            '''
 
+            echo $FILENAME
             
 
 
