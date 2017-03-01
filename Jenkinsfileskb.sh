b054742eb74a0bb068423dca7cb5ace2f5dabff3
Sakib-Ar-Rahman <sakib.ar.rahman@gmail.com>
2017-03-01 12:32:52 -0500

    Env Vars

--- Jenkinsfileskb.sh
+++ Jenkinsfileskb.sh
@@ -51,7 +51,7 @@ node {
             
             sh '''
             echo 'Existing Branch name'
-            echo $env.BRANCH_NAME
+            echo ${env.BRANCH_NAME}
 
             if [ -z "$BRANCH_NAME" ]; then
                 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
